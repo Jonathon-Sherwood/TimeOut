67d7847be92afbb5b5faa8695469be16d25976dc
Jonathon Sherwood <jonathondsherwood@hotmail.com>
2021-01-30 15:49:07 -0700

    Playground/Parking Colliders

---
+++ Assets/Scripts/Transition/Transition.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class Transition : MonoBehaviour
+{
+    public GameObject covers;
+    public GameObject hubcovers;
+    public PolygonCollider2D confiner;
+    public CinemachineConfiner cmConfiner;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            covers.SetActive(true);
+            cmConfiner.m_BoundingShape2D = confiner;
+            hubcovers.SetActive(false);
+
+        }
+    }
+}
