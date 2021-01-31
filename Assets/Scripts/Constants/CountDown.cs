80cc62e3e645c2b91dbe2dd94f041451ef4ad64a
Jonathon Sherwood <jonathondsherwood@hotmail.com>
2021-01-30 22:13:56 -0700

    Countdown Clock

---
+++ Assets/Scripts/Constants/CountDown.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDown : MonoBehaviour
+{
+    public float timeLeft = 120;
+    private float currentTimeLeft;
+    public Image waitCountdownImageThing;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTimeLeft = timeLeft;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTimeLeft -= Time.deltaTime;
+        waitCountdownImageThing.fillAmount = currentTimeLeft / timeLeft;
+        if (currentTimeLeft < 0)
+        {
+            print("Game Over");
+        }
+    }
+}
